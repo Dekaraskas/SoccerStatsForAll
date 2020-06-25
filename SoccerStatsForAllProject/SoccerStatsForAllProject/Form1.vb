7bc7dc088b48ad4bca92c3358da9e6a0b729cacc
Adam <dekaraskas@poczta.fm>
2020-06-25 10:42:58 +0200

    Ustawienie wyglądu okna głównego

--- SoccerStatsForAllProject/SoccerStatsForAllProject/Form1.vb
+++ SoccerStatsForAllProject/SoccerStatsForAllProject/Form1.vb
@@ -1,5 +1,16 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Text = "Simple Stats"
+
+
+
+        With Me
+
+            .Text = "Simple Stats"
+            .BackColor = Color.DarkGreen
+            .ForeColor = Color.White
+            .WindowState = FormWindowState.Maximized
+            .Font = New Font("Segoe UI", 9)
+
+        End With
     End Sub
 End Class
