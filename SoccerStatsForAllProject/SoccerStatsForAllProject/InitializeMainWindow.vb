dc3909abe76dde88d096f87a8c160f6f0c1d0b46
Adam <dekaraskas@poczta.fm>
2020-07-28 03:42:13 +0200

    Wywołanie funkcji tworzącej przyciski menu

--- SoccerStatsForAllProject/SoccerStatsForAllProject/InitializeMainWindow.vb
+++ SoccerStatsForAllProject/SoccerStatsForAllProject/InitializeMainWindow.vb
@@ -1,6 +1,13 @@
 ﻿Public Class InitializeMainWindow ' Ta klasa jest uruchamiana przy starcie programu
     Public Shared Sub ActionsWhenStart()
 
+
+
+        ' Stworzenie przycisków i dodanie ich do FlowMenuButtons w oknie głównym
+        MenuButtons.CreateMenuButtons()
+
+
+
         ' Dodanie do okna głównego FlowMenuButtons
         Form1.Controls.Add(AllControls.FlowMenuButtons)
 
