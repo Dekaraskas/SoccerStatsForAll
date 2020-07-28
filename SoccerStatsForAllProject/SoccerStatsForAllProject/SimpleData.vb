f4ed01d03688c464f6716903cda92545d436ad91
Adam <dekaraskas@poczta.fm>
2020-07-28 16:26:43 +0200

    Dodanie do bazy danych sześciu pierwszych drużyn

--- SoccerStatsForAllProject/SoccerStatsForAllProject/SimpleData.vb
+++ SoccerStatsForAllProject/SoccerStatsForAllProject/SimpleData.vb
@@ -1,3 +1,19 @@
 ﻿Public Class SimpleData ' Klasa ta zawiera przykładowe dane
+    Public Shared Sub Load()
 
+        With Collections.Teams
+
+            .Add(New TeamsClass With {.TeamID = 0, .TeamName = "Bayern"})
+            .Add(New TeamsClass With {.TeamID = 1, .TeamName = "Dortmund"})
+            .Add(New TeamsClass With {.TeamID = 2, .TeamName = "Leverkusen"})
+            .Add(New TeamsClass With {.TeamID = 3, .TeamName = "Schalke"})
+            .Add(New TeamsClass With {.TeamID = 4, .TeamName = "Lipsk"})
+            .Add(New TeamsClass With {.TeamID = 5, .TeamName = "Hoffenheim"})
+        End With
+
+
+
+
+
+    End Sub
 End Class
